67a8ab91090de74ac75ec837ce334002fdedac56
Luiz Guilherme Muniz barreto <166665419+luizguilh3rme@users.noreply.github.com>
2026-01-05 20:07:00 -0300

    att

--- estoque/src/pages/dashboard/new/index.tsx
+++ estoque/src/pages/dashboard/new/index.tsx
@@ -1,9 +1,29 @@
+import { Container } from "../../../components/container"
+import { DashboardHeader } from "../../../components/panelheader"
+
+import {FiUpload} from 'react-icons/fi'
+
 export function New() {
 
   return (
-    <>
-      <h1>Pagina cadastrar roteador</h1>
-    </>
+    <Container>
+      <DashboardHeader/>
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2">
+        <button className="border-2 w-48 rounded-lg flex items-center justify-center cursor-pointer border-gray-600 h-32 md:w-48">
+          <div className="absolute cursor-pointer">  
+            <FiUpload size={30} color="#000"/>
+          </div>
+          <div className="cursor-pointer">
+            <input type="file" accept="image/*" className="opacity-0 cursor-pointer" />
+          </div>
+        </button>
+      </div>
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2 mt-2">
+        <h1>TESTE</h1>
+      </div>
+    </Container>
   )
 }
 
